c8e533eaacbd7baa16a5ae72a262f8ea15f5c1ec
NacimBourelam <112643886+NacimBourelam@users.noreply.github.com>
2024-01-20 22:26:07 -0500

    Main modif graphe

--- traitement/main.py
+++ traitement/main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import zipfile
 import os
+import matplotlib.pyplot as plt
 
 #delete de Data.csv and DonneesBixi.csv if they exist
 if os.path.exists("traitement/Data.csv"):
@@ -43,12 +44,6 @@ df['ACHALENDEMENT'] = df['STARTSTATION_VISIT_COUNT'].apply(
     lambda x: 'low' if x < max_visit / 3 else 'average' if x < 2 * max_visit / 3 else 'high'
 )
 
-# Drop columns 
-df = df.drop(columns=['STARTSTATIONARRONDISSEMENT'])
-df = df.drop(columns=['ENDSTATIONNAME', 'ENDSTATIONLATITUDE', 'ENDSTATIONLONGITUDE'])
-df = df.drop(columns=['STARTTIMEMS','ENDTIMEMS','STARTSTATION_VISIT_COUNT'])
-df = df.drop(columns=['ENDSTATIONARRONDISSEMENT'])
-
 # sort by ACHALENDEMENT putting the ones with the ACHALENDEMENT 'high' first and the 'low' last
 achalandement_mapping = {'high': 3, 'average': 2, 'low': 1}
 df['ACHALENDEMENT_SORT'] = df['ACHALENDEMENT'].map(achalandement_mapping)
@@ -58,6 +53,40 @@ df = df.sort_values(by='ACHALENDEMENT_SORT', ascending=False)
 
 # Drop the temporary sorting column
 df = df.drop(columns=['ACHALENDEMENT_SORT'])
+
+# Filtrer les données pour les stations avec un achalandement 'high' et 'low'
+high_achalandement_df = df[df['ACHALENDEMENT'] == 'high']
+low_achalandement_df = df[df['ACHALENDEMENT'] == 'low']
+
+# Créer un graphique pour les stations avec un achalandement 'high'
+plt.figure(figsize=(10, 6))
+plt.hist(high_achalandement_df['STARTSTATIONNAME'], bins=30, color='green')
+plt.title('Nombre de Visites des Stations de Départ avec Achalandement Élevé')
+plt.xlabel('Stations de Départ')
+plt.ylabel('Nombre de Visites')
+plt.xticks(rotation=90)
+plt.tight_layout()
+# Sauvegarder le graphique
+plt.savefig('traitement/high_achalandement.png')
+plt.close()
+
+# Créer un graphique pour les stations avec un achalandement 'low'
+plt.figure(figsize=(10, 6))
+plt.hist(low_achalandement_df['STARTSTATIONNAME'], bins=30, color='red')
+plt.title('Nombre de Visites des Stations de Départ avec Achalandement Faible')
+plt.xlabel('Stations de Départ')
+plt.ylabel('Nombre de Visites')
+plt.xticks(rotation=90)
+plt.tight_layout()
+# Sauvegarder le graphique
+plt.savefig('traitement/low_achalandement.png')
+plt.close()
+
+# Drop columns 
+df = df.drop(columns=['STARTSTATIONARRONDISSEMENT'])
+df = df.drop(columns=['ENDSTATIONNAME', 'ENDSTATIONLATITUDE', 'ENDSTATIONLONGITUDE'])
+df = df.drop(columns=['STARTTIMEMS','ENDTIMEMS','STARTSTATION_VISIT_COUNT'])
+df = df.drop(columns=['ENDSTATIONARRONDISSEMENT'])
 
 # Save the final DataFrame to a new CSV file
 df.to_csv('traitement/DonneesBixi.csv', index=False)
